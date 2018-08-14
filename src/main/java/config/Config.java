5722d19b28c07ab6c8aec8e9dc41ce4a72109812
litianmin <litianmin@gmail.com>
2018-08-14 22:23:11 +0800

    fixed Config.java 的不使用的类

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,14 +1,10 @@
 package config;
 
-import com.alibaba.fastjson.JSON;
+
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-import event.EventBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.swing.*;
 import java.io.*;
 
 import java.util.ArrayList;
